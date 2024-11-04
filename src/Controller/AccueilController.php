2a60c88c7af75a3236f0d95ec66bb6e3196cee8d
Chris <chris93370@icloud.com>
2024-11-04 14:32:46 +0100

    first commit

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController{
+
+    #[Route('/', name: 'accueil')]
+    public function accueil(){
+        return $this->render('pages/index.html.twig');
+    }
+}
